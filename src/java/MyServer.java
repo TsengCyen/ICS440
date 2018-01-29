bb49237139e5e67ba434fe3af2d9801b08c6fc05
Unknown <tsengcyen@hotmail.com>
2018-01-29 15:14:53 -0600

    Fix'd Start

--- src/java/MyServer.java
+++ src/java/MyServer.java
@@ -1,23 +1,39 @@
 import java.io.*;
 import java.net.*;
+import java.util.logging.*;
 
 public class MyServer {
-    public final static int PORT = 13;
+    private final static int PORT = 13;
+    private final static Logger audit = Logger.getLogger("requests");
+    private final static Logger errors = Logger.getLogger("errors");
 
     public static void main(String[] args) {
         // Try to create a new ServerSocket
-        try(ServerSocket server = new ServerSocket(PORT)) {
+        try(DatagramSocket socket = new DatagramSocket(PORT)) {
             // Continuously listen to port 13 until connection is made
             while(true) {
+                System.out.println("Listening...");
+                try {
+                    DatagramPacket request = new DatagramPacket(new byte[1024], 1024);
+                    socket.receive(request);
 
-                System.out.println("[WORKING] - NoPort");
+                    // DoWork
 
-                try(Socket connection = server.accept()) {
-                    // DO WORK
+                    // -------------------------------------------------------
+                    // - Remove comments after DoWork is done                -
+                    // - Change <DATA> and <DATA_INFO> to appropriate values -
+                    // -------------------------------------------------------
+                    //DatagramPacket response - new DatagramPacket(<DATA>, <DATA>.length,
+                    //    resquest.getAddress(), request.getPort());
+                    //socket.send(response);
+                    //
+                    //audit.info(<DATA_INFO> + " sent to " + request.getAddress());
+                } catch (IOException e) {
+                    errors.log(Level.SEVERE, e.getMessage(), e);
                 }
             }
         } catch (IOException e) {
-            System.out.println(e);
+            errors.log(Level.SEVERE, e.getMessage(), e);
         }
     }
 }
