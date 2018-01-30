df7eb3d54788b2478a696049776e83f2c0668efc
Unknown <tsengcyen@hotmail.com>
2018-01-29 19:03:57 -0600

    Half way there... Maybe

--- src/java/MyServer.java
+++ src/java/MyServer.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
 import java.util.logging.*;
 
 public class MyServer {
@@ -8,9 +9,26 @@ public class MyServer {
     private final static Logger errors = Logger.getLogger("errors");
 
     public static void main(String[] args) {
+        // Allow user to input a file for program to find and use
+        // File MUST be inside the working project area
+        Scanner userInput = new Scanner(System.in);
+        String fileName = "notRealFile.name";
+        File file = new File(System.getProperty("user.dir") + "/" + fileName);
+
+        // Continue to prompt user until a valid file is found
+        while(!file.exists()) {
+            System.out.print("\nFilename (include extension): ");
+            fileName = userInput.nextLine();
+            file = new File(fileName);
+
+            if(!file.exists()) {
+                System.out.println("- File not found -");
+            }
+        }
+        userInput.close();
+
         // Try to create a new ServerSocket
         try(DatagramSocket socket = new DatagramSocket(PORT)) {
-            // Continuously listen to port 13 until connection is made
             while(true) {
                 System.out.println("Listening...");
                 try {
@@ -27,7 +45,7 @@ public class MyServer {
                     //    resquest.getAddress(), request.getPort());
                     //socket.send(response);
                     //
-                    //audit.info(<DATA_INFO> + " sent to " + request.getAddress());
+                    audit.info("<DATA_INFO>" + " sent to " + request.getAddress());
                 } catch (IOException e) {
                     errors.log(Level.SEVERE, e.getMessage(), e);
                 }
