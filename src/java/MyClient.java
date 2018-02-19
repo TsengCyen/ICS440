9d35d8b738f912fe79d92b6465718297afdd7737
Unknown <tsengcyen@hotmail.com>
2018-02-19 14:10:41 -0600

    New comments - Tried logging

--- src/java/MyClient.java
+++ src/java/MyClient.java
@@ -1,6 +1,7 @@
 import java.awt.image.*;
 import java.io.*;
 import java.net.*;
+import java.nio.charset.*;
 import java.util.*;
 
 import javax.imageio.*;
@@ -11,6 +12,7 @@ public class MyClient {
     private final static String HOSTNAME = "localhost";
 
     public static void main(String[] args) {
+        // Try to create a new ServerSocket through SERVER_PORT
         try(DatagramSocket socket = new DatagramSocket(CLIENT_PORT)) {
             // Allow user to input an image file for program to find and use
             // File MUST be inside the working project area
@@ -36,30 +38,42 @@ public class MyClient {
             ByteArrayOutputStream byteArrayOut = new ByteArrayOutputStream();
             ImageIO.write(bufferedImg, "jpg", byteArrayOut);
             byteArrayOut.flush();
-            byte[] dataNew = byteArrayOut.toByteArray();
-
-            /* OLD METHOD
-            BufferedImage bufferedImage = ImageIO.read(file);
-            WritableRaster rasterImage = bufferedImage.getRaster();
-            DataBufferByte imageData =
-                (DataBufferByte) rasterImage.getDataBuffer();
-
-            byte[] data = imageData.getData();
-            */
+            byte[] data = byteArrayOut.toByteArray();
 
             // Work with Server
             socket.setSoTimeout(10000);
             InetAddress host = InetAddress.getByName(HOSTNAME);
-            DatagramPacket request = new DatagramPacket(dataNew, dataNew.length,
+            DatagramPacket request = new DatagramPacket(data, data.length,
                 host, SERVER_PORT);
+
+            // Log datagrams being sent to console
+
+            /*
+             *
+             *
+             *
+             * getOffet() Not actually working. Only getting one instance.
+             * Assignment requires START BYTE OFFSET and END BYTE OFFSET
+             *
+             *
+             *
+             */
+
+            for(int i=0; i < data.length; i++) {
+                System.out.println("[" + data[i] + "]-[" +
+                    request.getOffset() + "]-[" + request.getOffset() + "]");
+            }
+
             socket.send(request);
 
+            // Retrieve Server reply
             byte[] data2 = new byte[1024];
             DatagramPacket response = new DatagramPacket(data2, data2.length,
                 host, SERVER_PORT);
             socket.receive(response);
 
-            System.out.println(response.getData());
+            // Output server response
+            System.out.println(new String(response.getData(), StandardCharsets.UTF_8));
 
         } catch ( IOException e ) {
             e.printStackTrace();
--- src/java/MyServer.java
+++ src/java/MyServer.java
@@ -4,59 +4,41 @@ import java.util.logging.*;
 
 public class MyServer {
     private final static int PORT = 13;
-    private final static Logger audit = Logger.getLogger("requests");
     private final static Logger errors = Logger.getLogger("errors");
 
     public static void main(String[] args) {
-        // Try to create a new ServerSocket
+        // Try to create a new ServerSocket through PORT
         try(DatagramSocket socket = new DatagramSocket(PORT)) {
             while(true) {
                 try {
+                    // Catch a sent packet
                     DatagramPacket request = new DatagramPacket(new byte[4096],
                         4096);
                     socket.receive(request);
 
+                    // Prepare output file for reconstruction
                     File fileOutput = new File(System.getProperty("user.dir") +
                         "fileOutput.png");
                     byte[] data = request.getData();
 
-
+                    // Convert data to image and output to prepared output file
                     ByteArrayInputStream imageByte = new ByteArrayInputStream(data);
                     InputStream inStream = imageByte;
                     OutputStream outStream = new FileOutputStream("outputfile.png");
 
-                    byte[] test = new byte[1024];
-                    int var;
-                    while((var = inStream.read(test)) > 0) {
-                        outStream.write(test, 0, var);
-                    }
-                    /*
-                    DataBuffer buffer = new DataBufferByte(data, data.length);
-                    WritableRaster raster =
-                        Raster.createInterleavedRaster(buffer, 100, 100, 2, 1,
-                        new int[]{0, 1, 2}, (Point) null);
-
-                    ColorModel cm = new ComponentColorModel(
-                        ColorModel.getRGBdefault().getColorSpace(),
-                        false, true, Transparency.OPAQUE, DataBuffer.TYPE_BYTE);
-
-                    BufferedImage image =
-                        new BufferedImage(cm, raster, true, null);
-
-                    ImageIO.write(imageByte, "png", fileOutput); */
-
-                    String responseText = "- Image File Processed -";
+                    // Do response
+                    String responseInfo = "- Image File Processed -";
                     DatagramPacket response = new DatagramPacket(
-                        responseText.getBytes(), responseText.length(),
+                        responseInfo.getBytes(), responseInfo.length(),
                         request.getAddress(), request.getPort());
-                    socket.send(request);
-                    audit.info(data + " sent to " + request.getAddress());
+                    socket.send(response);
+
                 } catch (IOException e) {
-                    errors.log(Level.SEVERE, e.getMessage(), e);
+                    e.printStackTrace();
                 }
             }
         } catch (IOException e) {
-            errors.log(Level.SEVERE, e.getMessage(), e);
+            e.printStackTrace();
         }
     }
 }
